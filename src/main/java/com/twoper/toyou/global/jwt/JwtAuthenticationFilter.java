9146690daa6c7996746e37f5e819ba3b3d9e49f1
yena5511 <ar01077422264@gmail.com>
2023-12-15 14:41:23 +0900

    수정

--- src/main/java/com/twoper/toyou/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/twoper/toyou/global/jwt/JwtAuthenticationFilter.java
@@ -1,102 +1,3 @@
-//package com.twoper.toyou.global.jwt;
-//
-//import com.auth0.jwt.JWT;
-//import com.auth0.jwt.algorithms.Algorithm;
-//import com.twoper.toyou.domain.user.model.dto.LoginRequestDto;
-//import com.twoper.toyou.global.jwt.JwtProperties;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.twoper.toyou.global.jwt.auth.PrincipalDetails;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.util.Date;
-//
-// //401오류
-//@RequiredArgsConstructor
-//public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
-//
-//    private final AuthenticationManager authenticationManager;
-//
-//    // Authentication 객체 만들어서 리턴 => 의존 : AuthenticationManager
-//    // 인증 요청시에 실행되는 함수 => /login
-//    @Override
-//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-//            throws AuthenticationException {
-//
-//        System.out.println("JwtAuthenticationFilter : 진입");
-//
-//        // request에 있는 username과 password를 파싱해서 자바 Object로 받기
-//        ObjectMapper om = new ObjectMapper();
-//        LoginRequestDto loginRequestDto = null;
-//        try {
-//            loginRequestDto = om.readValue(request.getInputStream(), LoginRequestDto.class);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("JwtAuthenticationFilter : "+loginRequestDto);
-//
-//        // 유저네임패스워드 토큰 생성
-//        UsernamePasswordAuthenticationToken authenticationToken =
-//                new UsernamePasswordAuthenticationToken(
-//                        loginRequestDto.getUsername(),
-//                        loginRequestDto.getPassword());
-//
-//        System.out.println("JwtAuthenticationFilter : 토큰생성완료");
-//
-//        // authenticate() 함수가 호출 되면 인증 프로바이더가 유저 디테일 서비스의
-//        // loadUserByUsername(토큰의 첫번째 파라메터) 를 호출하고
-//        // UserDetails를 리턴받아서 토큰의 두번째 파라메터(credential)과
-//        // UserDetails(DB값)의 getPassword()함수로 비교해서 동일하면
-//        // Authentication 객체를 만들어서 필터체인으로 리턴해준다.
-//
-//        // Tip: 인증 프로바이더의 디폴트 서비스는 UserDetailsService 타입
-//        // Tip: 인증 프로바이더의 디폴트 암호화 방식은 BCryptPasswordEncoder
-//        // 결론은 인증 프로바이더에게 알려줄 필요가 없음.
-//        Authentication authentication =
-//                authenticationManager.authenticate(authenticationToken);
-//
-//        PrincipalDetails principalDetailis = (PrincipalDetails) authentication.getPrincipal();
-//        System.out.println("Authentication : "+principalDetailis.getUser().getUsername());
-//        return authentication;
-//    }
-//
-//    // JWT Token 생성해서 response에 담아주기
-//    // JWT Token 생성해서 response에 담아주기
-//    @Override
-//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-//                                            Authentication authResult) throws IOException, ServletException {
-//
-//        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
-//
-//        String jwtToken = JWT.create()
-//                .withSubject(principalDetails.getUsername())
-//                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
-//                .withClaim("id", principalDetails.getUser().getId())
-//                .withClaim("username", principalDetails.getUser().getUsername())
-//                .sign(Algorithm.HMAC512(JwtProperties.SECRET));
-//
-//        System.out.println(jwtToken);
-//
-//        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
-//
-//        // /login 마치고 나면, 토큰을 body로 보내줌
-//        response.getWriter().write(String.valueOf(jwtToken));
-//    }
-//
-//
-//}
-//
-//
 package com.twoper.toyou.global.jwt;
 
 import com.auth0.jwt.JWT;
@@ -119,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 
+ //401오류
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
 
@@ -129,6 +31,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
             throws AuthenticationException {
+
         System.out.println("JwtAuthenticationFilter : 진입");
 
         // request에 있는 username과 password를 파싱해서 자바 Object로 받기
@@ -136,52 +39,60 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         LoginRequestDto loginRequestDto = null;
         try {
             loginRequestDto = om.readValue(request.getInputStream(), LoginRequestDto.class);
-            System.out.println(loginRequestDto);
-
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(loginRequestDto.getUsername(), loginRequestDto.getPassword());
-            Authentication authentication =
-                    authenticationManager.authenticate(authenticationToken);
-
-            PrincipalDetails principalDetailis = (PrincipalDetails) authentication.getPrincipal();
-            System.out.println("Authentication : " + principalDetailis.getUser().getUsername());
-            System.out.println("Username: " + loginRequestDto.getUsername());
-            System.out.println("Password: " + loginRequestDto.getPassword());
-
-            return authentication;
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+
+        System.out.println("JwtAuthenticationFilter : "+loginRequestDto);
+
+        // 유저네임패스워드 토큰 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        loginRequestDto.getUsername(),
+                        loginRequestDto.getPassword());
+
+        System.out.println("JwtAuthenticationFilter : 토큰생성완료");
+
+        // authenticate() 함수가 호출 되면 인증 프로바이더가 유저 디테일 서비스의
+        // loadUserByUsername(토큰의 첫번째 파라메터) 를 호출하고
+        // UserDetails를 리턴받아서 토큰의 두번째 파라메터(credential)과
+        // UserDetails(DB값)의 getPassword()함수로 비교해서 동일하면
+        // Authentication 객체를 만들어서 필터체인으로 리턴해준다.
+
+        // Tip: 인증 프로바이더의 디폴트 서비스는 UserDetailsService 타입
+        // Tip: 인증 프로바이더의 디폴트 암호화 방식은 BCryptPasswordEncoder
+        // 결론은 인증 프로바이더에게 알려줄 필요가 없음.
+        Authentication authentication =
+                authenticationManager.authenticate(authenticationToken);
+
+        PrincipalDetails principalDetailis = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("Authentication : "+principalDetailis.getUser().getUsername());
+        return authentication;
     }
 
+    // JWT Token 생성해서 response에 담아주기
+    // JWT Token 생성해서 response에 담아주기
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
 
-        System.out.println("successfulAuthentication 실행됨 : 인증이 완료되었다는 뜻임");
         PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
 
         String jwtToken = JWT.create()
-                .withSubject("cos 토큰")
-                .withExpiresAt(new Date(System.currentTimeMillis()+(60000*10)))
+                .withSubject(principalDetails.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
                 .withClaim("id", principalDetails.getUser().getId())
                 .withClaim("username", principalDetails.getUser().getUsername())
                 .sign(Algorithm.HMAC512(JwtProperties.SECRET));
 
         System.out.println(jwtToken);
 
-        response.addHeader("Authorization", "Bearer " + jwtToken);
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
 
         // /login 마치고 나면, 토큰을 body로 보내줌
         response.getWriter().write(String.valueOf(jwtToken));
     }
 
 
-
-
-
 }
-
 
